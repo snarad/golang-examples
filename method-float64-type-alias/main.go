47e98faf4127f916372a0872f360a9b4ea86fc96
Shivalik Narad <snarad@infoblox.com>
2019-01-07 10:24:30 -0800

    Added method float64 type alias example

---
+++ method-float64-type-alias/main.go
@@ -0,0 +1,21 @@
+// To define methods on a type you don’t “own”, you
+// need to define an alias for the type you want to extend
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+func main() {
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+}
