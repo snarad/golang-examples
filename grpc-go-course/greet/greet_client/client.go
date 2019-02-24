35d1de169a9dbbf796b870830eaa42976a846ef3
Shivalik Narad <snarad@infoblox.com>
2019-02-23 20:11:55 -0800

    Added Greet method in server and its unary invocation in client

--- grpc-go-course/greet/greet_client/client.go
+++ grpc-go-course/greet/greet_client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 
@@ -20,5 +21,21 @@ func main() {
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-	fmt.Printf("Created client: %f", c)
+	//fmt.Printf("Created client: %f", c)
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a Unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Shivalik",
+			LastName:  "Narad",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatal("error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v \n", res.Result)
 }
